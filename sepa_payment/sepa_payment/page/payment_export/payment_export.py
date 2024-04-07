aebf3dc416ac63cfc3e3dd6d5337905119c88821
viral <viral@fosserp.com>
2024-04-07 06:06:41 +0000

    UI of payment export and get draft payment entry on page

--- sepa_payment/sepa_payment/page/payment_export/payment_export.py
+++ sepa_payment/sepa_payment/page/payment_export/payment_export.py
@@ -15,14 +15,14 @@ from datetime import datetime
 @frappe.whitelist()
 def get_payments():
     payments = frappe.db.sql(
-        """ Select pe.name, pe.posting_date, pe.paid_amount, pe.party, pe.party_name, pe.paid_from, pe.paid_to_account_currency, per.reference_doctype,
-                                per.reference_name
-                                From `tabPayment Entry` as pe
-                                Left Join `tabPayment Entry Reference` as per ON per.parent = pe.name
-                                left join `tabBank Account` as ba ON ba.party_type = per.reference_doctype and ba.party = per.reference_name
-                                Where pe.docstatus = 0 and pe.payment_type = "Pay" and xml_file_generated = 0
-                                order by posting_date
-                                """,
+        """ Select pe.name, pe.posting_date, pe.paid_amount, pe.party, pe.party_name,
+            pe.paid_from, pe.paid_to_account_currency, per.reference_doctype, per.reference_name
+            From `tabPayment Entry` as pe
+            Left Join `tabPayment Entry Reference` as per ON per.parent = pe.name
+            left join `tabBank Account` as ba ON ba.party_type = per.reference_doctype and ba.party = per.reference_name
+            Where pe.docstatus = 0 and pe.payment_type = "Pay" and xml_file_generated = 0
+            order by posting_date
+        """,
         as_dict=1,
     )
 
